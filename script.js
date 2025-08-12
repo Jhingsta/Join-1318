f27231ebc8ab43d131bb5463dee8e6f37b5b1485
Tobias Spiller <t.spiller@netcologne.de>
2025-08-12 17:48:39 +0200

    started to implement firebase in script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ window.addEventListener('load', () => {
       splash.style.display = 'none';
     }, 2500);
   });
+
+  const BASE_URL = "https://join-1318-default-rtdb.europe-west1.firebasedatabase.app/";
+
+  let users = []; // Store users data
+
+  /**
+   * Loads users from the Firebase database and stores them in the users array.
+   * This function fetches contact information from the Firebase database
+   */
+  async function loadUsers() {
+    try {
+      const response = await fetch(`${BASE_URL}contacts.json`);
+    const data = await response.json();
+    users = data;
+  } catch (error) {
+    console.error("Error loading users:", error);
+  }
+}
