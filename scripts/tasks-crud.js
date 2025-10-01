7aab895ceab4c84c29c4e84a80ada280cd5352ca
Tobias Spiller <t.spiller@netcologne.de>
2025-10-01 17:07:45 +0200

    fix for const

--- scripts/tasks-crud.js
+++ scripts/tasks-crud.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://join-1318-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://join-1318-default-rtdb.europe-west1.firebasedatabase.app/";
 
 /**
  * Loads all tasks from the Firebase database.
